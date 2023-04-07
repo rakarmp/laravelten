be6a96ff7c9cb70ff7aa8311cf1664a1c10df4a4
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-04-06 22:17:35 -0700

    absurd

---
+++ resources/views/dokumentasi/index.blade.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menurut Saya</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css">
+    <link rel="stylesheet" href="scribbler-global.css">
+    <link rel="stylesheet" href="scribbler-landing.css">
+</head>
+
+<body>
+    <nav>
+        <div class="logo"></div>
+        <ul class="menu">
+            <div class="menu__item toggle"><span></span></div>
+            <li class="menu__item"><a href="/" class="link link--dark"><i class="fa fa-home"></i> Cabut</a></li>
+        </ul>
+    </nav>
+    <div class="hero">
+        <h1 class="hero__title">Laravel 10</h1>
+        <p class="hero__description">Tapi ngga tau upded apa aje</p>
+    </div>
+    <div class="hero__terminal">
+        <pre>
+        <!-- Place your demo code here -->
+        <code class="shell-session demo">#Solokeun, #Codingwithgabut, #CoffeCoding, #JavascriptMastery, #LinuxIndonesia</code>
+      </pre>
+    </div>
+    <div class="wrapper">
+        <div class="installation">
+            <h3 class="section__title">Install Laravel 10</h3>
+            <div class="tab__container">
+                <ul class="tab__menu">
+                    <li class="tab active" data-tab="mac">All User</li>
+                </ul>
+                <pre class="nohighlight code">
+            <code class="tab__pane active mac">$  composer create-project laravel/laravel nama_folder</code>
+          </pre>
+            </div>
+        </div>
+        <div class="feature">
+            <div class="feature__item">
+                <a href="https://laravel-news.com/laravel-10" target="_blank">
+                    <h3 class="section__title">Liat News Laravel 10</h3>
+                    <p>Selengkapnya kalo mau baca apa aja fitur baru nya yo baca</p>
+                </a>
+            </div>
+        </div>
+        <footer class="footer">SukaSuka build with gabut<a href="https://github.com/rakarmp" target="_blank"
+                class="link link--light">github</a>.</footer>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+        <script>
+            hljs.initHighlightingOnLoad();
+        </script>
+        <script src="scribbler.js"></script>
+</body>
+
+</html>
